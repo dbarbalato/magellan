7be1c34885723e26b3869808f55ff496598c14ad
D-Whiz <dbarbalato@gmail.com>
2013-03-10 01:07:29 -0500

    first commit

--- magellan.js
+++ magellan.js
@@ -1,5 +1,7 @@
-;
-(function() {
+// Magellan version 1.0
+// Provided by Dave Barbalato - https://github.com/dbarbalato/
+// Distributable under the MIT License
+;(function() {
 
     // Version identifier
     var VERSION = '1.0';
@@ -65,13 +67,16 @@
             if (direction) coordinate.direction = direction[0];
         }
 
-
+        // Format the current coordinate as Degrees Decimal
         magellan.toDD = function() {
             var decimal = coordinate.minutes / 60 + coordinate.seconds / 3600;
 
             var formatted;
-            if (coordinate.degrees > 0) formatted = (coordinate.degrees + decimal).toString();
-            else formatted = (coordinate.degrees - decimal).toString()
+            if (coordinate.degrees > 0) formatted = (coordinate.degrees + decimal)
+            else formatted = (coordinate.degrees - decimal)
+            
+            // Limit the precision to 4 decimal places
+            formatted = formatted.toFixed(4);
 
             if (coordinate.direction
                     && (coordinate.direction == SOUTH || coordinate.direction == WEST))
@@ -79,6 +84,7 @@
             return formatted;
         }
 
+        // Format the current coordinate as Degrees Minutes Seconds
         magellan.toDMS = function() {
             return new String(Math.abs(coordinate.degrees) + '°'
                 + coordinate.minutes + '\''
@@ -86,40 +92,54 @@
                 + (coordinate.direction ? coordinate.direction : '')).trim();
         }
 
-        // Treat the current coordinate as latitude
+        // Validate the current coordinate as latitude
         magellan.latitude = function() {
             // Coordinate is valid latitude if it exists and is between +/- 90
-            if (!coordinate || Math.abs(coordinate.degrees) > 180
+            if (coordinate && Math.abs(coordinate.degrees) <= 90
                     // and if it is equal to +/- 90, minutes and seconds are both 0
-                    || (Math.abs(coordinate.degrees) == 90 && (coordinate.minutes != 0 || coordinate.seconds != 0.0))
-                    // and the direction is North or South
-                    || coordinate.direction == EAST || coordinate.direction == WEST) {
+                    && (Math.abs(coordinate.degrees) != 90 || (coordinate.minutes == 0 && coordinate.seconds == 0.0))
+                    // and the minutes and seconds are both less than 60
+                    && (coordinate.minutes < 60 && coordinate.seconds < 60)
+                    // and if the coordinate direction is present, it is North or South
+                    && (!coordinate.direction || coordinate.direction == NORTH || coordinate.direction == SOUTH)) {
 
-                    // Something in the above validation logic failed, so return null
-                    return null;
+                    // In the event coordinate direction is null, we can automatically infer it
+                    // using the value of the degrees
+                    if (!coordinate.direction) coordinate.direction = coordinate.degrees > 0 ? NORTH : SOUTH
+
+                    // Enable method chaining
+                    return magellan;
             }
 
-            return magellan;
+            // In the event of a failure, break the chain, throwing an error
+            return null;
         }
 
-        // Treat the current coordinate as longitude
+        // Validate the current coordinate as longitude
         magellan.longitude = function() {
 
             // Coordinate is valid longitude if it exists and is between +/- 180
-            if (!coordinate || Math.abs(coordinate.degrees) > 180
+            if (coordinate && Math.abs(coordinate.degrees) <= 180
                     // and if it is equal to +/- 180, minutes and seconds are both 0
-                    || (Math.abs(coordinate.degrees) == 180 && (coordinate.minutes != 0 || coordinate.seconds != 0.0))
-                    // and the direction is East or West
-                    || coordinate.direction == NORTH || coordinate.direction == SOUTH) {
+                    && (Math.abs(coordinate.degrees) != 180 || (coordinate.minutes == 0 && coordinate.seconds == 0.0))
+                    // and the minutes and seconds are both less than 60
+                    && (coordinate.minutes < 60 && coordinate.seconds < 60)
+                    // and if the coordinate direction is present, it is East or West
+                    && (!coordinate.direction || coordinate.direction == EAST || coordinate.direction == WEST)) {
 
-                    // Something in the above validation logic failed, so return null
-                    return null;
+                    // In the event coordinate direction is null, we can automatically infer it
+                    // using the value of the degrees
+                    if (!coordinate.direction) coordinate.direction = coordinate.degrees > 0 ? EAST : WEST
+
+                    // Enable method chaining
+                    return magellan;
             }
 
-            // A-OK, so return magellan for chaining
-            return magellan;
+            // In the event of a failure, break the chain, throwing an error
+            return null;
         }
 
+        // Enable method chaining
         return magellan;
 
     }
