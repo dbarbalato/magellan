3a68a48d773c903a634a93f4f87a8f2a0f7e9103
D-Whiz <dbarbalato@gmail.com>
2013-03-10 00:37:15 -0500

    first commit

---
+++ magellan.js
@@ -0,0 +1,138 @@
+;
+(function() {
+
+    // Version identifier
+    var VERSION = '1.0';
+
+    // Compass direction constants
+    var NORTH = 'N';
+    var SOUTH = 'S';
+    var EAST = 'E';
+    var WEST = 'W';
+
+    // Signed degree format (e.g. -123.45)
+    var DD_FORMAT_REGEX = /^([+-]?\d{1,3})(.\d+)?$/
+
+    // Degrees minutes seconds format (e.g. 12°34'56" N or N12°34'56.123" )
+    var DMS_FORMAT_REGEX = /^[NSEW]?\s*(\d{1,3})°?(?:(\d{1,2})'?(?:(\d{1,2}(?:.\d+)?)"?)?)?\s*[NSEW]?$/
+    
+    // Magellan base function
+    function magellan() {
+
+        var coordinate = {};
+
+        // Handle function call when magellan( '-123', '45', '59' ) or similar
+        if (arguments.length >= 3) {
+            coordinate.degrees = parseInt(arguments[0]);
+            coordinate.minutes = parseInt(arguments[1]);
+            coordinate.seconds = parseFloat(arguments[2]);
+        }
+
+        // Handle function call when magellan(' 123°45'59" N ')
+        else if (arguments.length >= 1 && typeof arguments[0] == 'string') {
+            var matches;
+
+            //  Attempt to match against Decimal Degrees format
+            if ((matches = arguments[0].match(DD_FORMAT_REGEX)) != null) {
+                coordinate.degrees = parseInt(matches[1])
+
+                var decimal = parseFloat(matches[2]) || 0.0;
+                coordinate.minutes = parseInt(decimal * 60);
+                coordinate.seconds = parseFloat(((decimal * 60) - coordinate.minutes) * 60);
+
+            // Attempt to match against Degrees Minutes Seconds format
+            } else if ((matches = arguments[0].match(DMS_FORMAT_REGEX)) != null) {
+                coordinate.degrees = parseInt(matches[1]);
+                coordinate.minutes = parseInt(matches[2])|| 0;
+                coordinate.seconds = parseFloat(matches[3]) || 0.0;
+            }
+        } 
+        
+        // Handle function call when magellan( 123.4567 ) or similar
+        else if (arguments.length >= 1 && typeof arguments[0] == 'number') {
+
+            // Degrees is the integer portion of the input
+            coordinate.degrees = parseInt(arguments[0]);
+            
+            var decimal = Math.abs(parseFloat(arguments[0]) - coordinate.degrees);
+            coordinate.minutes = parseInt(decimal * 60);
+            coordinate.seconds = parseFloat(((decimal * 60) - coordinate.minutes) * 60);
+        } 
+
+        // Attempt to determine the direction if it was supplied
+        if (typeof arguments[arguments.length - 1] === 'string') {
+            var direction = arguments[arguments.length - 1].toUpperCase().match(/[NSEW]/);
+            if (direction) coordinate.direction = direction[0];
+        }
+
+
+        magellan.toDD = function() {
+            var decimal = coordinate.minutes / 60 + coordinate.seconds / 3600;
+
+            var formatted;
+            if (coordinate.degrees > 0) formatted = (coordinate.degrees + decimal).toString();
+            else formatted = (coordinate.degrees - decimal).toString()
+
+            if (coordinate.direction
+                    && (coordinate.direction == SOUTH || coordinate.direction == WEST))
+                    formatted = '-' + formatted;
+            return formatted;
+        }
+
+        magellan.toDMS = function() {
+            return new String(Math.abs(coordinate.degrees) + '°'
+                + coordinate.minutes + '\''
+                + coordinate.seconds.toFixed(4) + '" '
+                + (coordinate.direction ? coordinate.direction : '')).trim();
+        }
+
+        // Treat the current coordinate as latitude
+        magellan.latitude = function() {
+            // Coordinate is valid latitude if it exists and is between +/- 90
+            if (!coordinate || Math.abs(coordinate.degrees) > 180
+                    // and if it is equal to +/- 90, minutes and seconds are both 0
+                    || (Math.abs(coordinate.degrees) == 90 && (coordinate.minutes != 0 || coordinate.seconds != 0.0))
+                    // and the direction is North or South
+                    || coordinate.direction == EAST || coordinate.direction == WEST) {
+
+                    // Something in the above validation logic failed, so return null
+                    return null;
+            }
+
+            return magellan;
+        }
+
+        // Treat the current coordinate as longitude
+        magellan.longitude = function() {
+
+            // Coordinate is valid longitude if it exists and is between +/- 180
+            if (!coordinate || Math.abs(coordinate.degrees) > 180
+                    // and if it is equal to +/- 180, minutes and seconds are both 0
+                    || (Math.abs(coordinate.degrees) == 180 && (coordinate.minutes != 0 || coordinate.seconds != 0.0))
+                    // and the direction is East or West
+                    || coordinate.direction == NORTH || coordinate.direction == SOUTH) {
+
+                    // Something in the above validation logic failed, so return null
+                    return null;
+            }
+
+            // A-OK, so return magellan for chaining
+            return magellan;
+        }
+
+        return magellan;
+
+    }
+
+    // Expose the version
+    magellan.version = 'Version ' + VERSION;
+
+    // Publish the library, either as an AMD module or to the window
+    if (typeof define === 'function' && define.amd) {
+        define('magellan', function() {
+            return magellan;
+        });
+    } else {
+        window.magellan = magellan;
+    }
+})();
